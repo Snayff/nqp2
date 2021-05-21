9a20b5b2294a44d137caf62a2697f70c198aa238
Josh Snaith <snaith2@gmail.com>
2021-05-21 19:44:22 +0100

    setup image loading on init

--- scripts/core/assets.py
+++ scripts/core/assets.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
 import logging
+import os
 from typing import List, TYPE_CHECKING
 
 import pygame
 
-from scripts.core.constants import ASSET_PATH, DEFAULT_IMAGE_SIZE, IMAGE_NOT_FOUND_PATH
+from scripts.core.constants import ASSET_PATH, DATA_PATH, DEFAULT_IMAGE_SIZE, IMAGE_NOT_FOUND_PATH
 from scripts.ui_elements.text import Font
 
 if TYPE_CHECKING:
@@ -26,19 +27,21 @@ class Assets:
             "default": Font(str(ASSET_PATH / "fonts/small_font.png"), (255, 255, 255)),
         }
 
-        # used to hold images called during runtime so only one copy ever exists.
-        self.images: Dict[str, pygame.Surface] = {}
+        # used to hold images so only one copy per dimension ever exists.
+        self.images: Dict[str, Dict[str, pygame.Surface]] = self._load_images()
+
 
     def get_image(
         self,
-        img_path: str,
+        folder: str,
+        image_name: str,
         desired_dimensions: Tuple[int, int] = (DEFAULT_IMAGE_SIZE, DEFAULT_IMAGE_SIZE),
         copy: bool = False,
     ) -> pygame.Surface:
         """
         Get the specified image and resize if dimensions provided. Dimensions are in (width, height) format. If img
-        path is "none" then a blank surface is created to the size of the desired dimensions, or TILE_SIZE if no
-        dimensions provided.
+        path is "none" then a blank surface is created to the size of the desired dimensions, or DEFAULT_IMAGE_SIZE
+        if no dimensions provided.
         """
         # ensure numbers arent negative
         if desired_dimensions[0] <= 0 or desired_dimensions[1] <= 0:
@@ -93,3 +96,37 @@ class Assets:
             images.append(self.get_image(path, desired_dimensions, copy))
 
         return images
+
+    @staticmethod
+    def _load_images() -> Dict[str, Dict[str, pygame.Surface]]:
+        """
+        Load all images by folder.
+        """
+        images = {}
+
+        folders = [
+            "nodes"
+        ]
+
+        for folder in folders:
+            path = ASSET_PATH / folder
+            images[folder] = {}
+            for image_name in os.listdir(path):
+                image = pygame.image.load(str(path / image_name)).convert_alpha()
+                width = image.get_width()
+                height = image.get_height()
+                images[folder][f"{image_name.split('.')[0]}@{width}x{height}"] = image  # split to remove extension
+
+        # add not found image to debug
+        images["debug"] = {}
+        image = pygame.image.load(str(ASSET_PATH / "debug/image_not_found.png")).convert_alpha()
+        width = image.get_width()
+        height = image.get_height()
+        images["debug"][f"not_found@{width}x{height}"] = image
+        # add transparent surface to debug
+        image = pygame.Surface((DEFAULT_IMAGE_SIZE, DEFAULT_IMAGE_SIZE))
+        image.set_alpha(0)
+        images["debug"][f"blank@{DEFAULT_IMAGE_SIZE}x{DEFAULT_IMAGE_SIZE}"] = image
+
+        return images
+
