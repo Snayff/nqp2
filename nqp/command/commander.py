39a3de5ad86ca215782193d34584b2d3da43b247
Leif Theden <leif.theden@gmail.com>
2022-05-08 10:22:03 -0500

    re-add commander items

--- nqp/command/commander.py
+++ nqp/command/commander.py
@@ -21,4 +21,4 @@ class Commander:
 
         data = self._game.data.commanders[type_]
         self.name: str = data["name"]
-        # self.items: List[Item] = [create_item(game.data, name) for name in data["items"]]
+        self.items: List[Item] = [game.data.create_item(name) for name in data["items"]]
